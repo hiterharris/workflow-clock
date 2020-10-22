9d1414ff00b16fe51d580345f2dc95bf1e8efb7e
Hiter Harris <hiterharris@comcast.net>
2020-10-22 14:00:50 -0400

    hide start when playing, clean code

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,31 +6,48 @@ const Timer = () => {
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const [renderStart, setRenderStart] = useState('');
   const [audio] = useState(new Audio(alert));
-  const [playing] = useState(false);
+  const [playing, setPlaying] = useState(false);
 
-  // const togglePlay = () => setPlaying(!playing);
-
-  useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
-    [playing, audio]
-  );
-
-  const toggle = () => {
+  const toggleActive = () => {
     setIsActive(!isActive);
+  }
+
+  const handleMinutes = (e) => {
+    e.preventDefault();
+    setMinutes(e.target.value);
+  }
+
+  const handleSeconds = (e) => {
+    e.preventDefault();
+    setSeconds(e.target.value);
+  }
+
+  const showZero = () => {
+    if (seconds < 10) {
+      return '0';
+    }
+  }
+
+  const showInput = () => {
+    if (!isActive) {
+      return (
+        <>
+          <input type="text" placeholder='Minutes' onChange={handleMinutes} />
+          <input type="text" placeholder='Seconds' onChange={handleSeconds} />
+        </>
+      );
+    } else {
+      return null
+    }
   }
 
   const reset = () => {
     setSeconds(0);
     setMinutes(0);
     setIsActive(false);
-    audio.pause();
-  }
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setMinutes(e.target.value);
+    setPlaying(false);
   }
 
   useEffect(() => {
@@ -43,16 +60,11 @@ const Timer = () => {
     if (isActive && seconds === 0) {
         setSeconds(59);
     }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
-
-  useEffect(() => {
-    let interval = null;
     if (isActive && seconds === 0) {
         setMinutes(minutes - 1);
     }
     if (isActive && minutes === 0 && seconds === 0) {
-        audio.play();
+        setPlaying(true);
         setSeconds(0);
         setMinutes(0);
         setIsActive(false);
@@ -60,34 +72,24 @@ const Timer = () => {
     return () => clearInterval(interval);
   }, [isActive, minutes, seconds, audio]);
 
-  const zero = () => {
-    if (seconds < 10) {
-      return '0';
-    }
-  }
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing, audio]);
 
-  const showTime = () => {
-    if (isActive && (minutes > 0 || seconds > 0)) {
-      return <div className="time">{minutes}:{zero()}{seconds}</div>
+  useEffect(() => {
+    if (playing) {
+      setRenderStart('hide');
     } else {
-      return null
+      setRenderStart('');
     }
-  }
-
-  const showInput = () => {
-    if (!isActive) {
-      return <input type="text" onChange={handleChange} />
-    } else {
-      return null
-    }
-  }
+  }, [playing])
 
   return (
     <div className="app">
-        {showInput()}
-        {showTime()}
+      {showInput()}
+      <div className="time">{minutes}:{showZero()}{seconds}</div>
       <div className="row">
-        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'} ${renderStart} `} onClick={toggleActive}>
           {isActive ? 'Pause' : 'Start'}
         </button>
         <button className="button" onClick={reset}>
